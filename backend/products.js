66c07d3b162e90ff61a27e271203e2a7388d20b3
kehinde_Daniels <kehindedaniels501@gmail.com>
2024-07-06 10:25:01 +0100

    added backend, installed radux toolkit and axios

---
+++ backend/products.js
@@ -0,0 +1,36 @@
+const products = [
+  {
+    id: 1,
+    name: "name-1",
+    description: "description-1",
+    images:
+      "https://res.cloudinary.com/dk47zom8d/image/upload/v1720256996/paul-gaudriault-a-QH9MAAVNI-unsplash_q832ux.jpg",
+    price: 50,
+  },
+  {
+    id: 2,
+    name: "name-2",
+    description: "description-2",
+    images:
+      "https://res.cloudinary.com/dk47zom8d/image/upload/v1720256990/domino-studio-164_6wVEHfI-unsplash_lyc0s7.jpg",
+    price: 150,
+  },
+  {
+    id: 3,
+    name: "name-3",
+    description: "description-3",
+    images:
+      "https://res.cloudinary.com/dk47zom8d/image/upload/v1720256986/the-dk-photography-NUoPWImmjCU-unsplash_fryfwj.jpg",
+    price: 250,
+  },
+  {
+    id: 4,
+    name: "name-4",
+    description: "description-4",
+    images:
+      "https://res.cloudinary.com/dk47zom8d/image/upload/v1720256976/usama-akram-kP6knT7tjn4-unsplash_ezkx2s.jpg",
+    price: 350,
+  },
+];
+
+module.exports = products;
